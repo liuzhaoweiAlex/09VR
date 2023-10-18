7c30a925ad55a736bf2c99f9b8e13ee97624d45d
liuzhaoweiAlex <458549644@qq.com>
2023-10-18 16:50:14 +0800

    新增内容
    1.修复投掷无重力的问题
    2.在玩家位置随机生成勇者
    3.勇者在碰到地面和对boss造成伤害时销毁

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public GameObject character; // 勇者的引用
+    public float minHeightY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DestroySelf();
+    }
+
+    /// <summary>
+    /// 低于一定高度自毁
+    /// </summary>
+    void DestroySelf()
+    {
+        if(character.transform.position.y < minHeightY)
+        {
+            Destroy(character);
+        }
+    }
+
+    /// <summary>
+    /// 攻击自毁
+    /// </summary>
+    public void HitDestroy()
+    {
+        Destroy(character);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponentInParent<BossComponent>() != null)
+        {
+            Debug.Log("计算伤害");
+            HitDestroy(); // 攻击自毁
+        }
+        else
+        {
+            Debug.Log("不是目标");
+        }
+    }
+}
