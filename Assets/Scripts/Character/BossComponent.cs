21fc1c9d4b0313ad7c9632c4f59a005043ed3f31
Yoky <2429856747@qq.com>
2023-10-20 14:41:46 +0800

    bugfix: 修复ui显示bug，完成勇者碰撞伤害结算功能

--- Assets/Scripts/Character/BossComponent.cs
+++ Assets/Scripts/Character/BossComponent.cs
@@ -10,14 +10,15 @@ public class BossComponent : MonoBehaviour
     public float currentHealth;
     public float defensePower = 5f;
     public float attackPower = 10f;
+    public float CharacterDamage = 50f;
     public GameObject win;
 
-    private Image healthBarImage; // 血条图片
+    public Image healthBarImage; // 血条图片
 
     void Start()
     {
         currentHealth = maxHealth;
-        healthBarImage = GetComponentInChildren<Image>();
+        //healthBarImage = GetComponentInChildren<Image>();
     }
 
     public void TakeDamage(float damage)
@@ -33,12 +34,28 @@ public class BossComponent : MonoBehaviour
         {
             win.SetActive(true);
         }
+
+        Debug.Log("防御：" + defensePower);
+        Debug.Log("真实造成伤害：" + actuallDamage);
+        Debug.Log("当前血量：" + currentHealth);
     }
 
     void UpdateHealthBar()
     {
         float fillAmount = currentHealth / maxHealth;
         healthBarImage.fillAmount = fillAmount;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponentInParent<Character>() != null)
+        {
+            Debug.Log("攻击伤害："+ CharacterDamage);
+            TakeDamage(CharacterDamage);
+
+
+        }
+
     }
 }
 
